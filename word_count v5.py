c9fa15ba7a3a1bf10ccd5c3869dfe8492457c6d4
dg1245 <qitiandalong@gmail.com>
2023-11-12 19:10:51 +0800

    Update word_count v5.py

--- word_count v5.py
+++ word_count v5.py
@@ -7,7 +7,7 @@
 # 使用说明
 # 先去亚马逊后台->Brands->Brand Analytics -> Search Analytics->
 # ->Top Search Terms->Generate Report->Download Manager->下载csv
-# 第21行代码填入wanted_word，运行代码即可
+# 第21行代码填入wanted_words，运行代码即可
 # result文件夹里会生成2个分别包含word_count和keyword_rank的csv文件
 
 import os
